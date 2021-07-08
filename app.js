1a9e1042179a2787380b66d54a2af7589864d97e
Ashley Sulaiman <ashley.sulaiman83@gmail.com>
2021-07-08 11:58:11 -0400

    Completed Exercise

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(number) {
+    return ( => (plusNumber += number);
+        )
+}
+plus15 = plus(15);
+
+console.log(plus15(10));
+
